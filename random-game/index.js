939312fd969dc493948cdc2fbdc4ac3053498bf8
DialecticalLaw <d.shmuratkin@mail.ru>
2023-10-01 12:00:17 +0300

    refactor: change the unit of measurement of movement

--- random-game/index.js
+++ random-game/index.js
@@ -1,11 +1,23 @@
-const plates = document.querySelectorAll('.plate');
 const btn = document.querySelector('.btn-new-game');
+const playBoard = document.querySelector('.play-board');
+
 
 btn.addEventListener('click', startGame);
 
 startGame();
 
-function makePoint(num, elem) {
+function startGame() {
+    const plates = document.querySelectorAll('.plate');
+    for (let plate of plates) {
+        plate.remove();
+    }
+
+    const shiftValues = ['0%', '25%', '50%', '75%'];
+    playBoard.insertAdjacentHTML('beforeend', `<div class="plate" style="left:${shiftValues[Math.floor(Math.random() * 4)]};top:${shiftValues[Math.floor(Math.random() * 4)]};background-color: aqua;">2</div>`);
+    savePlateCoordinates(document.querySelector('.plate'));
+}
+
+function makePlate(elem, num) {
     elem.innerHTML = num;
     if (num === 2) {
         elem.style['background-color'] = 'aqua';
@@ -22,19 +34,30 @@ function makePoint(num, elem) {
     }
 }
 
-function startGame() {
-    for (let plate of plates) {
-        plate.innerHTML = '';
-        plate.style['background-color'] = '';
+function savePlateCoordinates(elem) {
+    if (elem.offsetLeft / playBoard.offsetWidth * 100 + '%' === '0%') {
+        elem.classList.add('x0');
+    } else if (elem.offsetLeft / playBoard.offsetWidth * 100 + '%' === '25%') {
+        elem.classList.add('x1');
+    } else if (elem.offsetLeft / playBoard.offsetWidth * 100 + '%' === '50%') {
+        elem.classList.add('x2');
+    } else if (elem.offsetLeft / playBoard.offsetWidth * 100 + '%' === '75%') {
+        elem.classList.add('x3');
     }
-    const numRandomPlate = Math.floor(Math.random() * 16);
-    makePoint(2, plates[numRandomPlate]);
+
+    if (elem.offsetTop / playBoard.offsetWidth * 100 + '%' === '0%') {
+        elem.classList.add('y0');
+    } else if (elem.offsetTop / playBoard.offsetWidth * 100 + '%' === '25%') {
+        elem.classList.add('y1');
+    } else if (elem.offsetTop / playBoard.offsetWidth * 100 + '%' === '50%') {
+        elem.classList.add('y2');
+    } else if (elem.offsetTop / playBoard.offsetWidth * 100 + '%' === '75%') {
+        elem.classList.add('y3');
+    }
 }
 
-document.addEventListener('keydown', movePoints);
+document.addEventListener('keydown', movePlates);
 
-function movePoints(event) {
-    if (event.code === 'ArrowRight') {
-        
-    }
+function movePlates(event) {
+    
 }
